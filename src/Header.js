28fc1673aa0f3d24b51d69d2875448f2c4ca986e
Kwame <gtwene001@st.ug.edu.gh>
2021-11-23 11:24:44 +0000

    Header Complete

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,43 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import "../src/Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
-    <div>
-      <h1>This is my header</h1>
-
+    <div className="header">
       {/* Left Header */}
       <div className="header__left">
-        <img src="" alt="" />
+        <img
+          src="https://seeklogo.com/images/L/linkedin-icon-logo-FBADE03110-seeklogo.com.png"
+          alt=""
+        />
       </div>
 
       {/* Search Header */}
       <div className="header__search">
         {/* SearchIcon */}
+        <SearchIcon />
         <input type="text" />
       </div>
 
       {/* Right Header */}
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://i.dlpng.com/static/png/6860651_preview.png"
+          title="me"
+        />
+      </div>
     </div>
   );
 }
